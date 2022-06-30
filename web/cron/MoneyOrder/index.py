6f7a15a9522f2361e979f8bb7b0b81a3a9ad5bca
新井耀介 <k016c1200@it-neec.jp>
2022-06-30 12:21:18 +0900

    commit

--- web/cron/MoneyOrder/index.py
+++ web/cron/MoneyOrder/index.py
@@ -29,7 +29,7 @@ def today(pair):
     today = today.strftime('%Y-%m-%d')
 
     # 為替pairを所定の形に変更
-    code = pair
+    code = f'{pair}'
     # dataの取得
     data = web.get_data_yahoo(code, today)
     # 最終日（今日）の終値（リアルタイム値）を返す
@@ -44,7 +44,7 @@ def week(pair):
     end = end.strftime('%Y-%m-%d')
 
      # 為替pairを所定の形に変更
-    code = pair
+    code = f'{pair}'
 
     # dataの取得
     data = web.get_data_yahoo(code, end, start)
@@ -56,14 +56,16 @@ def img(pair):
 
    start = datetime.date(2015,1,1)
    end = datetime.date.today()
+   code = f'{pair}'
+   img = f'../../img/{pair}.png'
 
-   data_YD = web.DataReader('^DJI', 'yahoo', start, end) #get data
+   data_YD = web.DataReader(code, 'yahoo', start, end) #get data
 
-   plt.plot(data_YD['Adj Close'],label='DJI')
-   plt.title("DJI")
+   plt.plot(data_YD['Adj Close'],label=pair)
+   plt.title(pair)
    plt.xlabel('date')
-   plt.ylabel('DJI')
-   plt.savefig("../../img/DJI.png")
+   plt.ylabel(pair)
+   plt.savefig(img)
    plt.legend()
    plt.show()
 
@@ -71,32 +73,31 @@ def img(pair):
 
 
 
-conn = pm.connect(**DATABASE)
-cursor = conn.cursor()
+data = ["AUDJPY=X", "CADJPY=X", "^DJI", "EURJPY=X", "GBPJPY=X","^GSPC","^HSI","^KS11","^NDX","^N225","^NSEI","^RUT","USDJPY=X","^VIX"]
 
-sql = ("SELECT count(1) as count FROM moneyorder WHERE code_name=%s")
-cursor.execute(sql, 'DJI')
-result = cursor.fetchone()
+for item in data:
 
-TODAY = today('^DJI')
-WEEK = week('^DJI')
-result2 = img('DJI')
+    conn = pm.connect(**DATABASE)
+    cursor = conn.cursor()
 
-if result['count'] == 0:
-    sql = ('INSERT INTO moneyorder (code_name, today, week) VALUES (%s, %s, %s)')
-    cursor.execute(sql, ('DJI', TODAY, WEEK))
+    sql = ("SELECT count(1) as count FROM moneyorder WHERE code_name=%s")
+    cursor.execute(sql, item)
+    result = cursor.fetchone()
+
+    WEEK = week(item)
+    TODAY = today(item)
+    result2 = img(item)
+
+    rate = ((TODAY - WEEK) / TODAY) * 100
+    soaring = format(rate, '.2f')
+
+    if result['count'] == 0:
+        sql = ('INSERT INTO moneyorder (code_name, today, week, rate ) VALUES (%s, %s, %s, %s)')
+        cursor.execute(sql, (item, TODAY, WEEK, soaring))
+        conn.commit()
+    else:
+        sql = ('UPDATE moneyorder SET today = %s, week = %s , rate = %s  ,create_at = %s WHERE code_name = %s')
+        cursor.execute(sql, (TODAY, WEEK, soaring,time.strftime('%Y-%m-%d %H:%M:%S'), item))
+
     conn.commit()
-else:
-    sql = ('UPDATE moneyorder SET today = %s, week = %s , create_at = %s WHERE code_name = %s')
-    cursor.execute(sql, (TODAY, WEEK, time.strftime('%Y-%m-%d %H:%M:%S'), 'DJI'))
-
-conn.commit()
 conn.close()
-
-
-
-
-# -----------------------------------------------------------------------------------
-# Symble指定は "JPY=X" でも "DJI=X" でも同じ。 開始日/終了日は date型でも指定可能
-#  https://info.finance.yahoo.co.jp/fx/detail/?code=EURJPY=FX
-# -----------------------------------------------------------------------------------
